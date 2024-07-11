7f33745fc744dd19fdbb7e87b08286cf0bbaf944
Alonso Vazquez <haloanderson45@gmail.com>
2024-07-11 14:48:55 -0600

    make the Delete link interactive and remove items in the checkout page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,7 @@ cart.forEach((cartItem) => {
   );
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -99,5 +99,8 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const { productId } = link.dataset;
     removeFromCart(productId);
+
+    const containerElement = document.querySelector(`.js-cart-item-container-${productId}`);
+    containerElement.remove();
   });
 });
