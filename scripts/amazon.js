1ce10c68cc82e863a5c87d87f3c85673f3f93e44
Alonso Vazquez <haloanderson45@gmail.com>
2024-07-11 13:41:33 -0600

    move addToCart into cart.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let productsHTML = "";
@@ -59,46 +59,40 @@ products.forEach((product) => {
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
+
+function displayAddedToCartMessage(productId) {
+  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+  let timeoutId;
+
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+
+  addedMessage.classList.add("added-to-cart-visible");
+  timeoutId = setTimeout(() => {
+    addedMessage.classList.remove("added-to-cart-visible");
+  }, 2000);
+}
+
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const { productId } = button.dataset;
-    const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
-    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
-    let matchingItem;
-    
-    cart.forEach((item) => {
-        if (productId === item.productId) {
-            matchingItem = item;
-        }
-    });
+    const quantity = Number(
+      document.querySelector(`.js-quantity-selector-${productId}`).value
+    );
 
-    if (matchingItem) {
-        matchingItem.quantity += quantity;
-    } else {
-        cart.push({
-            productId,
-            quantity
-        });
-    }
+    addToCart(productId, quantity);
 
-    let timeoutId;
+    displayAddedToCartMessage(productId);
 
-    if (timeoutId) {
-        clearTimeout(timeoutId);
-    }
-
-    addedMessage.classList.add('added-to-cart-visible');
-    timeoutId = setTimeout(() => {
-        addedMessage.classList.remove('added-to-cart-visible');
-    }, 2000);
-
-    let cartQuantity = 0;
-
-    cart.forEach((item) => {
-        cartQuantity += item.quantity;
-    });
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-
-    console.log(cart);
+    updateCartQuantity();
   });
 });
