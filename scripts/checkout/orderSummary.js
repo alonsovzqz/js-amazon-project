65fb49eac6b521ec47e06d90cc80e35711d9b14b
Alonso Vazquez <haloanderson45@gmail.com>
2024-07-11 21:26:48 -0600

    make paymentSummary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -8,6 +8,8 @@ import {
 import { products, getProduct } from "../../data/products.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 
+import { renderPaymentSummary } from "./paymentSummary.js";
+
 import { formatCurrency } from "../utils/money.js";
 
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
@@ -130,6 +132,8 @@ export function renderOrderSummary() {
       );
       containerElement.remove();
       updateCartQuantity();
+
+      renderPaymentSummary();
     });
   });
 
@@ -177,6 +181,7 @@ export function renderOrderSummary() {
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 }
